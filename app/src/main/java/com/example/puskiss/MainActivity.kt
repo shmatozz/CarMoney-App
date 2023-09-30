5eeffe4de05974e8cf514174c55f4abc7b6dc5be
MaffyMaths <matvey33.baryshev@mail.ru>
2023-09-30 18:40:03 +0300

    add deposits percents addition every day

--- app/src/main/java/com/example/puskiss/MainActivity.kt
+++ app/src/main/java/com/example/puskiss/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.puskiss
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,13 +11,21 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.puskiss.databinding.ActivityMainBinding
+import java.time.LocalDateTime
+import java.util.Calendar
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkLastVisit()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -32,4 +43,68 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    private fun checkLastVisit() {
+        preferences = getSharedPreferences("stats", Context.MODE_PRIVATE)
+        val lastVisitDay = preferences.getInt("LAST_VISIT_DAY", 0)
+        val lastVisitMonth = preferences.getInt("LAST_VISIT_MONTH", 0)
+
+        val calendar = Calendar.getInstance()
+        val current = LocalDateTime.of(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH),
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            calendar.get(Calendar.SECOND)
+        )
+
+        if ((current.dayOfMonth - lastVisitDay) + (current.month.value - lastVisitMonth) != 0) {
+            val daysPass: Int
+            if (current.dayOfMonth > lastVisitDay) {
+                daysPass = current.dayOfMonth - lastVisitDay
+            } else {
+                daysPass = current.dayOfMonth + (current.month.length(false) - lastVisitDay)
+            }
+
+            addPercentageToDeposit(daysPass)
+            moveToDeposit()
+
+            val editor = preferences.edit()
+            editor.putInt("LAST_VISIT_DAY",  current.dayOfMonth)
+            editor.putInt("LAST_VISIT_MONTH",  current.month.value)
+            editor.apply()
+        }
+    }
+
+    private fun addPercentageToDeposit(dayPass: Int) {
+        val countDeposits = preferences.getInt("DEPOSIT_COUNT", 0)
+
+        for (i in 0 until countDeposits) {
+            val curPercentage = (1.05).pow(dayPass).toFloat()
+            var curSum = preferences.getFloat("DEPOSIT_SUM%s".format(i), 0F)
+            curSum = (curSum * curPercentage * 100).roundToInt().toFloat() / 100
+
+            val editor = preferences.edit()
+            editor.putFloat("DEPOSIT_SUM%s".format(i),  curSum)
+            editor.apply()
+        }
+    }
+
+    private fun moveToDeposit() {
+        val lastTotalCount = preferences.getInt("TOTAL", 0)
+
+        if (preferences.getInt("DEPOSIT_COUNT", 0) != 0) {
+            val currentSumDeposit = preferences.getFloat("DEPOSIT_SUM0", 0F)
+
+            val editor = preferences.edit()
+            editor.putFloat("DEPOSIT_SUM0", currentSumDeposit + lastTotalCount)
+            editor.putInt("TOTAL", 0)
+            editor.putInt("YELLOW", 0)
+            editor.putInt("GREEN", 0)
+            editor.putInt("ORANGE", 0)
+            editor.apply()
+        }
+    }
+
 }
